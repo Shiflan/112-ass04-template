e370e5b41d50836e68e26d5a8aa40cf6ac99cc04
Jordan Shiflett <jshiflett@mtech.edu>
2026-09-21 17:29:51 -0600

    Why not

--- src/code.c
+++ src/code.c
@@ -468,7 +468,7 @@ char * format_my_pow(char dest[], int r)
     
     clear_string(dest, 64);
 
-    sprintf(dest, "%-12d", r);
+    sprintf(dest, "pow = %-12d", r);
     
     return dest;
 }
@@ -495,15 +495,15 @@ char * format_my_pow_double(char dest[], double r)
 {
     clear_string(dest, 64);
     if (r < 10)
-        sprintf(dest, "%012.9f", r);
+        sprintf(dest, "pow = %012.9f", r);
     else if (r < 100)
-        sprintf(dest, "%012.8f", r);
+        sprintf(dest, "pow = %012.8f", r);
     else if (r < 1000)
-        sprintf(dest, "%012.7f", r);
+        sprintf(dest, "pow = %012.7f", r);
     else if (r < 10000)
-        sprintf(dest, "%012.6f", r);
+        sprintf(dest, "pow = %012.6f", r);
     else
-        sprintf(dest, "%012.5f", r);
+        sprintf(dest, "pow = %012.5f", r);
     return dest;
 }
 
